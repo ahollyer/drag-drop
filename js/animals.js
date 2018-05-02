24a248c99bcbac1b30262fa8b315850cfa12bea8
Aspen Hollyer <aspen.dollins@gmail.com>
2018-05-02 05:37:34 -0500

    Add basic tutorial

--- js/animals.js
+++ js/animals.js
@@ -4,18 +4,35 @@
 const ANIMALS = {
   animal_giraffe: {
     evolvesTo: "animal_penguin",
-    pts: 100
+    pts: 100,
+    discovered: true
   },
   animal_penguin: {
     evolvesTo: "animal_bear",
-    pts: 150
+    pts: 150,
+    discovered: false
   },
   animal_bear: {
     evolvesTo: "animal_crocodile",
-    pts: 200
+    pts: 200,
+    discovered: false
   },
   animal_crocodile: {
     evolvesTo: "animal_chicken",
-    pts: 300
+    pts: 300,
+    discovered: false
+  },
+};
+
+const TUTORIAL_ANIMALS = {
+  tutorial_giraffe: {
+    evolvesTo: "tutorial_penguin",
+    pts: 100,
+    discovered: true
+  },
+  tutorial_penguin: {
+    evolvesTo: "tutorial_penguin",
+    pts: 100,
+    discovered: true
   },
 };
--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 // TODO: Make animals move around
 // TODO: Add basic store functionality
-// TODO:
+// TODO: Add a discovered: false flag to animals so we can display a popup the first time
+// TODO: Finish tutorial
+// TODO: Consider breaking up functions further
 
 /****************************************************
   Set up game variables
 ****************************************************/
-var gameBox = document.getElementById("game-box");
-var scoreBoard = document.getElementById("scoreboard");
-// The user begins the game with 3 giraffes (see index.html)
+const GAMEBOARD = document.getElementById("gameboard");
+const SCOREBOARD = document.getElementById("scoreboard");
+// The user begins the game with 3 giraffes, a 10-animal
+// limit, and a new animal every 5 seconds.
 var animalCounter = 3;
-// To start, the user is allowed up to 10 animals at a time
 var maxAnimals = 10;
-// Initialize the score counter
+var makeAnimalInterval = 5000;
 var score = 0;
 
 /****************************************************
@@ -20,10 +22,10 @@ var score = 0;
 function updateScore(pts, span) {
   score += pts;
   // Update scoreboard at top of screen
-  scoreBoard.innerText = score;
-  // Flash points earned with CSS animation (see main.css)
-  span.classList.add("fade-in-out")
-  span.textContent = `+${pts}`;
+  SCOREBOARD.innerText = score;
+  // In span, flash points earned with CSS animation
+  span.classList.add("fade-in-out");
+  span.textContent = `+$${pts}`;
 }
 
 /****************************************************
@@ -45,16 +47,37 @@ function makeAnimal(animalType) {
   return newAnimal;
 }
 
-// Add a new animal every 5 seconds until maxAnimals count is reached
+// Add a new animal every interval until maxAnimals count is reached
 function startPopulating() {
   setInterval(function() {
     if(animalCounter < maxAnimals) {
-      gameBox.appendChild(makeAnimal("animal_giraffe"));
+      GAMEBOARD.appendChild(makeAnimal("animal_giraffe"));
       animalCounter++;
     }
-  }, 5000);
+  }, makeAnimalInterval);
 }
+// TODO: Run this only once the tutorial window is closed.
 startPopulating();
+
+/****************************************************
+  Move animals randomly around the gameboard
+****************************************************/
+// TODO: Trap animals into bounding box
+var bobbleAnimal = document.getElementsByClassName(".bobble");
+
+function setProperty(tx, ty, tz) {
+  bobbleAnimal.style.transform = `translate3d(${tx}px, ${ty}px, ${tz}px)`;
+}
+
+function changeTranslateValues() {
+  // Random number from -100 to 100
+  var x = Math.floor(Math.random() * 201) - 100;
+  var y = Math.floor(Math.random() * 201) - 100;
+  var z = Math.floor(Math.random() * 201) - 100;
+  setProperty(x, y, z);
+}
+
+// setInterval(changeTranslateValues, 500);
 
 /****************************************************
   Make animals draggable - uses Interact.js
@@ -62,7 +85,7 @@ startPopulating();
 interact('.draggable').draggable({
     // Enable inertial throwing
     inertia: true,
-    // Keep animals within their parental div (#game-box)
+    // Keep animals within their parental div (#gameboard)
     restrict: {
       restriction: "parent",
       endOnly: true,
@@ -132,21 +155,6 @@ interact('.animal_crocodile').dropzone({
   },
 });
 
-interact('.tutorial_giraffe').dropzone({
-  accept: '.tutorial_giraffe',
-  overlap: OVERLAP_AMOUNT,
-  ondrop: function (event) {
-    removeAnimal(event.relatedTarget);
-    evolveAnimal(event.target);
-    clearGameChanges();
-  },
-});
-
-function clearGameChanges() {
-  score = 0;
-  animalCounter = 3;
-}
-
 /****************************************************
   Allow animals to be removed and evolved
 ****************************************************/
@@ -157,7 +165,7 @@ function removeAnimal(animalDiv) {
 
 function evolveAnimal(animalDiv) {
   animalDiv.classList.forEach(c => {
-    if(c.includes('animal')) {
+    if(c.includes("animal")) {
       // Get animal object from classname in animal div
       var animalToEvolve = ANIMALS[c];
       // Change div class & img src to next evolutionary animal
@@ -171,3 +179,59 @@ function evolveAnimal(animalDiv) {
     }
   });
 }
+
+/****************************************************
+  Tutorial at start of game
+****************************************************/
+var tutorialStep = 0;
+var instruction = document.getElementById('tutorial-instruction');
+var dismissButton = document.getElementById('tutorial-dismiss');
+
+function updateTutorial() {
+  tutorialStep++;
+  switch(tutorialStep) {
+    case 1:
+      var arrow = document.querySelector(".tutorial-arrow");
+      arrow.parentNode.removeChild(arrow);
+      instruction.innerText = "Great! Match animals to make new critters (and earn money).";
+      dismissButton.style.display = "inline-block";
+      break;
+    default:
+      instruction.innerText = "Animal Fuser";
+  }
+}
+
+function removeTutorialAnimal(animalDiv) {
+  animalDiv.parentNode.removeChild(animalDiv);
+}
+
+function evolveTutorialAnimal(animalDiv) {
+  animalDiv.classList.forEach(c => {
+    if(c.includes("tutorial")) {
+      // Get animal object from classname in animal div
+      var animalToEvolve = TUTORIAL_ANIMALS[c];
+      // Change div class & img src to next evolutionary animal
+      animalDiv.classList.replace(c, animalToEvolve.evolvesTo)
+      var evolvesToImg = animalToEvolve.evolvesTo.replace('tutorial', 'animal');
+      console.log('evolvesToImg', evolvesToImg);
+      animalDiv.lastElementChild.setAttribute("src", `img/${evolvesToImg}.png`)
+      // Reset point display span & update score
+      var oldSpan = animalDiv.firstElementChild;
+      var newSpan = document.createElement("span");
+      animalDiv.replaceChild(newSpan, oldSpan);
+      // Even though it's just the tutorial, we'll let the user
+      // earn points for fusing the tutorial animals
+      updateScore(animalToEvolve.pts, newSpan);
+    }
+  });
+}
+
+interact('.tutorial_giraffe').dropzone({
+  accept: '.tutorial_giraffe',
+  overlap: OVERLAP_AMOUNT,
+  ondrop: function (event) {
+    removeTutorialAnimal(event.relatedTarget);
+    evolveTutorialAnimal(event.target);
+    updateTutorial();
+  },
+});
